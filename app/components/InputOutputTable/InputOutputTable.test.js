14c55acc1c9112a4864825ffa9b2495903556ee6
timwekkenbc <tim.vanderwekken@gov.bc.ca>
2024-05-06 08:34:37 -0700

    Added unit testing libraries/configuration

---
+++ app/components/InputOutputTable/InputOutputTable.test.js
@@ -0,0 +1,20 @@
+// NOTE: this is just a placeholder test file, it should be updated with actual tests
+import * as React from "react";
+import { render } from "@testing-library/react";
+import InputOutputTable from "./InputOutputTable";
+
+describe("InputOutputTable", () => {
+  beforeAll(() => {
+    Object.defineProperty(window, "matchMedia", {
+      writable: true,
+      value: jest.fn().mockImplementation((query) => ({
+        addListener: jest.fn(),
+        removeListener: jest.fn(),
+      })),
+    });
+  });
+
+  test("renders InputOutputTable component", async () => {
+    render(<InputOutputTable />);
+  });
+});
