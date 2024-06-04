79358e0e4ff68ea9125e416ad7697d77db0583c7
timwekkenbc <tim.vanderwekken@gov.bc.ca>
2024-06-04 16:12:57 -0700

    Updated how API is called so it can support both client-side and server-side calls in a secure way

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+  async rewrites() {
+    // Proxy API requests to the backend at server when in development
+    // In dev/production, this is setup on OpenShift
+    if (process.env.NODE_ENV === "development") {
+      return [
+        {
+          source: "/api/:path*",
+          destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
+        },
+      ];
+    } else {
+      return [];
+    }
+  },
+};
 
 export default nextConfig;
