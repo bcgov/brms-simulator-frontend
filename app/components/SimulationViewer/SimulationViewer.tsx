49467ceedf6c90af7ae87f8b3d49dd0db65905c1
brysonjbest <bryson@Brysons-Laptop.local>
2024-06-17 11:47:26 -0700

    Update with tab view.

--- app/components/SimulationViewer/SimulationViewer.tsx
+++ app/components/SimulationViewer/SimulationViewer.tsx
@@ -2,7 +2,8 @@
 import React, { useState, useEffect, useRef } from "react";
 import dynamic from "next/dynamic";
 import Link from "next/link";
-import { Flex, Button } from "antd";
+import { Flex, Button, Tabs } from "antd";
+import type { TabsProps } from "antd";
 import { ExportOutlined } from "@ant-design/icons";
 import { SubmissionData } from "../../types/submission";
 import SubmissionSelector from "../SubmissionSelector";
@@ -15,6 +16,7 @@ import ScenarioViewer from "../ScenarioViewer/ScenarioViewer";
 // Need to disable SSR when loading this component so it works properly
 const RulesDecisionGraph = dynamic(() => import("../RulesDecisionGraph"), { ssr: false });
 
+const { TabPane } = Tabs;
 interface SimulationViewerProps {
   jsonFile: string;
   chefsFormId: string;
@@ -38,6 +40,7 @@ export default function SimulationViewer({ jsonFile, chefsFormId, rulemap, scena
   const [contextToSimulate, setContextToSimulate] = useState<SubmissionData | null>();
   const [outputSchema, setOutputSchema] = useState<Record<string, any> | null>(ruleMapOutputs);
   const [resultsOfSimulation, setResultsOfSimulation] = useState<Record<string, any> | null>();
+  const [activeTab, setActiveTab] = useState("scenarios");
   const [resetTrigger, setResetTrigger] = useState<boolean>(false);
   const simulateButtonRef = useRef<HTMLButtonElement>(null);
   const [manualOrPredefined, setManualOrPredefined] = useState<boolean>(false);
@@ -56,11 +59,69 @@ export default function SimulationViewer({ jsonFile, chefsFormId, rulemap, scena
   useEffect(() => {
     // reset context/results when a new submission is selected
     resetContextAndResults();
-    console.log(selectedSubmissionInputs, "this is submissionn inputs updating");
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [selectedSubmissionInputs]);
 
   useEffect(() => {}, [resultsOfSimulation]);
+
+  const handleTabChange = (key: string) => {
+    if (key === "reset") {
+      handleReset();
+    }
+  };
+
+  const handleReset = () => {
+    setSelectedSubmissionInputs(ruleMapInputs);
+    setResetTrigger((prev) => !prev);
+  };
+
+  const scenarioTab = (
+    <ScenarioViewer
+      scenarios={scenarios}
+      setSelectedSubmissionInputs={setSelectedSubmissionInputs}
+      resultsOfSimulation={resultsOfSimulation}
+      runSimulation={runSimulation}
+    />
+  );
+
+  const manualInputTab = (
+    <Flex gap="middle">
+      {selectedSubmissionInputs && (
+        <Flex vertical gap={"small"} align="end" className={styles.inputSection}>
+          <InputOutputTable
+            title="Inputs"
+            rawData={selectedSubmissionInputs}
+            setRawData={setSelectedSubmissionInputs}
+            submitButtonRef={simulateButtonRef}
+          />
+          <Button ref={simulateButtonRef} size="large" type="primary" onClick={runSimulation}>
+            Simulate ▶
+          </Button>
+        </Flex>
+      )}
+      {outputSchema && <InputOutputTable title="Outputs" rawData={outputSchema} setRawData={setOutputSchema} />}
+      {resultsOfSimulation && <InputOutputTable title="Results" rawData={resultsOfSimulation} />}
+    </Flex>
+  );
+
+  const resetTab = (
+    <Button onClick={handleReset} size="large" type="primary">
+      Reset ↻
+    </Button>
+  );
+
+  const items: TabsProps["items"] = [
+    {
+      key: "1",
+      label: "Simulate pre-defined test scenarios",
+      children: scenarioTab,
+    },
+    {
+      key: "2",
+      label: "Simulate inputs manually",
+      children: manualInputTab,
+    },
+  ];
 
   return (
     <Flex gap="large" vertical>
@@ -74,102 +135,7 @@ export default function SimulationViewer({ jsonFile, chefsFormId, rulemap, scena
       </div>
       <Flex justify="space-between" align="center" className={styles.contentSection}>
         <Flex gap="middle">
-          <Button
-            size="large"
-            type="default"
-            onClick={() => {
-              setManualOrPredefined((prev) => !prev);
-              setSelectedSubmissionInputs(ruleMapInputs);
-              setResetTrigger((prev) => !prev);
-            }}
-          >
-            Switch
-          </Button>
-          <Button
-            size="large"
-            type="default"
-            onClick={() => {
-              setSelectedSubmissionInputs(ruleMapInputs);
-              setResetTrigger((prev) => !prev);
-            }}
-          >
-            Reset ↻
-          </Button>
-        </Flex>
-      </Flex>
-
-      {/* 
-      <Flex justify="space-between" align="center" className={styles.contentSection}>
-        <Flex gap="middle">
-          <SubmissionSelector
-            chefsFormId={chefsFormId}
-            setSelectedSubmissionInputs={setSelectedSubmissionInputs}
-            resetTrigger={resetTrigger}
-          />
-          {selectedSubmissionInputs && (
-            <>
-              <Button ref={simulateButtonRef} size="large" type="primary" onClick={runSimulation}>
-                Simulate ▶
-              </Button>
-              <Button
-                size="large"
-                type="default"
-                onClick={() => {
-                  setSelectedSubmissionInputs(ruleMapInputs);
-                  setResetTrigger((prev) => !prev);
-                }}
-              >
-                Reset ↻
-              </Button>
-            </>
-          )}
-        </Flex>
-        <Link href={`https://submit.digital.gov.bc.ca/app/form/submit?f=${chefsFormId}`} target="_blank">
-          <Button>
-            Submission form <ExportOutlined />
-          </Button>
-        </Link>
-      </Flex>
-      <Flex gap="middle" wrap="wrap" className={styles.contentSection}>
-        {selectedSubmissionInputs && (
-          <InputOutputTable
-            title="Inputs"
-            rawData={selectedSubmissionInputs}
-            setRawData={setSelectedSubmissionInputs}
-            submitButtonRef={simulateButtonRef}
-          />
-        )}
-        {outputSchema && <InputOutputTable title="Outputs" rawData={outputSchema} setRawData={setOutputSchema} />}
-        {resultsOfSimulation && <InputOutputTable title="Results" rawData={resultsOfSimulation} />}
-      </Flex> */}
-      <Flex justify="space-between" align="center" className={styles.contentSection}>
-        <Flex gap="middle">
-          {manualOrPredefined ? (
-            <>
-              {selectedSubmissionInputs && (
-                <Flex vertical gap={"small"} align="end" className={styles.inputSection}>
-                  <InputOutputTable
-                    title="Inputs"
-                    rawData={selectedSubmissionInputs}
-                    setRawData={setSelectedSubmissionInputs}
-                    submitButtonRef={simulateButtonRef}
-                  />
-                  <Button ref={simulateButtonRef} size="large" type="primary" onClick={runSimulation}>
-                    Simulate ▶
-                  </Button>
-                </Flex>
-              )}
-              {outputSchema && <InputOutputTable title="Outputs" rawData={outputSchema} setRawData={setOutputSchema} />}
-              {resultsOfSimulation && <InputOutputTable title="Results" rawData={resultsOfSimulation} />}
-            </>
-          ) : (
-            <ScenarioViewer
-              scenarios={scenarios}
-              setSelectedSubmissionInputs={setSelectedSubmissionInputs}
-              resultsOfSimulation={resultsOfSimulation}
-              runSimulation={runSimulation}
-            />
-          )}
+          <Tabs defaultActiveKey="1" items={items} onChange={handleTabChange} tabBarExtraContent={resetTab}></Tabs>
         </Flex>
       </Flex>
     </Flex>
